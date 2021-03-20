ce0383b44b26a468cb26f7cbf0816c36c917869b
Josa Gesell <josa@gesell.me>
2021-03-20 11:54:10 +0100

    Fix conflict with telescope

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.16
 
 require (
 	github.com/josa42/go-gitignore v1.0.0
-	github.com/josa42/go-neovim v0.3.0
+	github.com/josa42/go-neovim v0.3.1
 )
 
 // replace github.com/josa42/go-neovim => ../go-neovim
--- run/build.sh
+++ run/build.sh
@@ -6,8 +6,8 @@ uuid='2f4eab2fca750d49cc9039bac724b89b'
 go run -ldflags "-X main.uuid=${uuid}" ./pkg --manifest tree -location plugin/tree.vim
 
 eval $(go env | grep 'GOARCH\|GOOS')
-export GOOS="$1"
-export GOARCH="$2"
+export GOOS="${1:-$GOOS}"
+export GOARCH="${2:-$GOARCH}"
 
 go build -ldflags "-X main.uuid=${uuid}" -o bin/tree-${GOOS}-${GOARCH} ./pkg
 
